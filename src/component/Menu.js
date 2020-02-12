8601b103b383284eb741a64dc73a4455d46f7f57
SanghunShin <tlstkdgns95@gmail.com>
2020-02-12 23:20:23 +0900

    menu.js에 from, to 를 업데이트 하기 성공

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,39 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Station from './Station';
+import Reservation from './Reservation';
+
 
 import './Menu.css';
 
 const Menu = () => {
+    
+    const [from, setFrom] = useState();
+    const [to, setTo] = useState();
+    console.log("from:",from);
+    console.log("to:",to);
+    
+
     return (
-        <div className = "menu-wrapper">
-            <fieldlist className = "station-wrapper">
-                <legend className = "menu-title">승차권 예매하기</legend>
-                <ul className = "menu-ul">
-                    <li className = "li-wrapper">
-                        <label className = "menu-leg"> 출발역 </label>
-                        <Station/>
-                    </li>
-                </ul>
-                <ul>
-                    <li className = "li-wrapper">
-                        <label className = "menu-leg"> 도착역 </label>
-                        <Station/>
-                    </li>
-                </ul>
-                <ul>
-                    <li className = "li-wrapper">
-                        <label className = "date-wrapper">출발일 </label>
+        <>
+            <div className = "menu-wrapper">
+                <fieldlist className = "station-wrapper">
+                    <legend className = "menu-title">승차권 예매하기</legend>
+                    <ul className = "menu-ul">
+                        <li className = "li-wrapper">
+                            <label className = "menu-leg"> 출발역 </label>
+                            <Station setStation = {setFrom}></Station>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li className = "li-wrapper">
+                            <label className = "menu-leg"> 도착역 </label>
+                            <Station setStation = {setTo}></Station>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li className = "li-wrapper">
+                            <label className = "date-wrapper">출발일 </label>
                             <input className = "menu-input-date" type = "date" name = "date"/>
-                    </li>
-                </ul>
-                <ul>
-                    <li className= "li-wrapper">
-                        <label className = "time-wrapper">시간</label>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li className= "li-wrapper">
+                            <label className = "time-wrapper">시간</label>
                             <input className = "menu-input-time" type = "time" name = "time"/>
-                    </li>
-                </ul>
-            </fieldlist>
-        </div>
+                        </li>
+                    </ul>
+                </fieldlist>
+            </div>
+            <div>
+                <Reservation/>
+            </div>
+        </>
     )
 }
 
--- src/component/Station.js
+++ src/component/Station.js
@@ -35,15 +35,15 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-const Station = () => {
+const Station = (props) => {
 
+    
     const [names, setNames] = useState([]); //기차역 전부다 때려박은거
     const [isModalOpen, toggleModal] = useState(false); 
     const [stationList, setStationList] = useState([[]]); // 초성별 기차역 배열
     const [initialList, setInitialList] = useState([]); //초성 리스트
     const [initial, setInitial] = useState(0); //초성 선택
-    const [from, SetFrom] = useState();
-    const [to,SetTo] = useState();
+    // const [station, setStation] = useState();
 
     const onClick = async () => {
 
@@ -78,7 +78,6 @@ const Station = () => {
             }
 
             setNames(_names);
-            console.log("eEEEE");
 
         } catch (e) {
             console.log(e);
@@ -95,41 +94,43 @@ const Station = () => {
       getListboxProps,
       getOptionProps,
       groupedOptions,
+      value,
     } = useAutocomplete({
     //   id: 'use-autocomplete-demo',
       options: names,
       getOptionLabel: option => option,
     });
-  
-
-    const nameList = names.map((name,index) => <option key = { index } > { name } </option>);
+    
+    // const nameList = names.map((name,index) => <option key = { index } > { name } </option>);
     const ButtonList = initialList.map((ini,index) => <button className = "right-margin" key = { index } onClick = { () => { setInitial(index) } }> { ini } </button>  );
-    const selectList = stationList[initial].map((station, index) => <option key = {index}> { station } </option>);
+    const selectList = stationList[initial].map((cstation, index) => <option key = {index} value = {cstation}> { cstation } </option>);
+    
 
     return ( 
         <div>
-            <div>
-                <div {...getRootProps()}>
-                    <label className = {classes.label} {...getInputLabelProps()}>
-                    </label>
-                    <input onClick = { onSearch } className = {classes.input} {...getInputProps()} />
-                </div>
-                    {groupedOptions.length > 0 ? (
-                        <ul className = {classes.listbox} {...getListboxProps()}>
-                        {groupedOptions.map((option, index) => (
-                            <li {...getOptionProps({ option, index })}>{option}</li>
-                        ))}
-                        </ul>
-                    ) : null}
-            </div>
-
             <div valgn="top" className = "sel-box">
+                <div>
+                    <div {...getRootProps()}>
+                        <label className = {classes.label} {...getInputLabelProps()}>
+                        </label>
+                        <input onClick = { onSearch }  {...getInputProps()} />
+                    </div >
+                        {groupedOptions.length > 0 ? (
+                            <ul className = {classes.listbox} {...getListboxProps()}>
+                                {groupedOptions.map((option, index) => (
+                                    <li {...getOptionProps({ option, index})} > { option } </li>
+                                ))}
+                            </ul>
+                        ): null}
+                </div>
+
                 <button onClick = { onClick } > 검색 </button>
                 <Modal isOpen = { isModalOpen } toggle = { toggleModal } >
                     <h1>기차역</h1>
                         { ButtonList }
                     <div className = "top-margin">
-                        <select>
+                        {/* <select onChange={(e) => setStation(e.target.value)}>  */}
+                        <select onChange={(e) => props.setStation(e.target.value)}> 
                             { selectList }
                         </select> 
                     </div>
